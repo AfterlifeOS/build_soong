7372fbe6af31781c4b6400dca57167153eff7584
Romeo <rbesar210311@gmail.com>
2025-12-14 11:45:48 +0000

    soong: Bring Back Afterlife Dumpvar display
    Change-Id: I9674cea291fbcc10475691802e82487c3d0906e6
    Signed-off-by: Romeo <rbesar210311@gmail.com>

--- ui/build/dumpvars.go
+++ ui/build/dumpvars.go
@@ -182,7 +182,13 @@ var BannerVars = []string{
 func Banner(config Config, make_vars map[string]string) string {
 	b := &bytes.Buffer{}
 
-	fmt.Fprintln(b, "============================================")
+	fmt.Fprintln(b, "=======================================================")
+	fmt.Fprintln(b, " █████████████████████████████████████████████████████ ")
+	fmt.Fprintln(b, " ██▀▄─██▄─▄▄─█─▄─▄─█▄─▄▄─█▄─▄▄▀█▄─▄███▄─▄█▄─▄▄─█▄─▄▄─█ ")
+	fmt.Fprintln(b, " ██─▀─███─▄█████─████─▄█▀██─▄─▄██─██▀██─███─▄████─▄█▀█ ")
+	fmt.Fprintln(b, " ▀▄▄▀▄▄▀▄▄▄▀▀▀▀▄▄▄▀▀▄▄▄▄▄▀▄▄▀▄▄▀▄▄▄▄▄▀▄▄▄▀▄▄▄▀▀▀▄▄▄▄▄▀ ")
+	fmt.Fprintln(b, "=======================================================")
+
 	for _, name := range BannerVars {
 		if make_vars[name] != "" {
 			fmt.Fprintf(b, "%s=%s\n", name, make_vars[name])
@@ -195,22 +201,19 @@ func Banner(config Config, make_vars map[string]string) string {
 		}
 	}
 
-	// Only show USE_RBE and USE_REWRAPPER when the user has explicitly set SOONG_NINJA
 	if config.ninjaCommand != NINJA_DEFAULT {
 		fmt.Fprintf(b, "SOONG_NINJA=%s\n", config.ninjaCommand.String())
 		fmt.Fprintf(b, "USE_RBE=%t\n", config.UseRBE())
 		fmt.Fprintf(b, "USE_REWRAPPER=%t\n", config.UseRewrapper())
 	}
 
-	// Normally config.soongOnlyRequested already takes into account PRODUCT_SOONG_ONLY,
-	// except when doing `get_build_var report_config`, which is run during envsetup.
 	if config.skipKatiControlledByFlags {
 		fmt.Fprintf(b, "SOONG_ONLY=%t\n", config.soongOnlyRequested)
-	} else { // default for this product
+	} else {
 		fmt.Fprintf(b, "SOONG_ONLY=%t\n", make_vars["PRODUCT_SOONG_ONLY"] == "true")
 	}
 
-	fmt.Fprint(b, "============================================")
+	fmt.Fprintln(b, "=======================================================")
 
 	return b.String()
 }
